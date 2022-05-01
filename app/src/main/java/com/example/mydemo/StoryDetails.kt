4bfc7150d5ae7d23a22ad388489f48f34c2aeaf0
Mussa-Adolai <60032727+Mussa-Adolai@users.noreply.github.com>
2022-05-01 23:54:49 +0300

    StoryDetail2

--- app/src/main/java/com/example/mydemo/StoryDetails.kt
+++ app/src/main/java/com/example/mydemo/StoryDetails.kt
@@ -1,11 +1,26 @@
 package com.example.mydemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toolbar
 
 class StoryDetails : AppCompatActivity() {
+    lateinit var toolBarD :androidx.appcompat.widget.Toolbar
+    lateinit var backArrowDet:ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_story_details)
+
+        toolBarD = findViewById(R.id.tbarD)
+        backArrowDet = findViewById(R.id.bArrD)
+
+        backArrowDet.setOnClickListener(){
+            var d = Intent(this,MainActivity::class.java)
+            startActivity(d)
+        }
+
     }
 }
